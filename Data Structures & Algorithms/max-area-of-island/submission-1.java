dc995d1d0f6c2557fb6c4c8668ddfc68b02676ac
Sree Preetham <76846180+sree-preetham@users.noreply.github.com>
2026-04-27 13:53:05 +0900

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.java
@@ -0,0 +1,60 @@
+class Cell {
+    public int row;
+    public int col;
+    public Cell(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+}
+class Solution {
+    private boolean check(int row, int col, int[][] grid){
+        return row >=0 && col>=0 && row < grid.length && col < grid[0].length && grid[row][col] == 1;
+    }
+    private int gridBfs(int[][] grid, int row, int col, ArrayDeque<Cell> queue){
+        queue.offerLast(new Cell(row, col));
+        grid[row][col] = '.';
+        int[] dirX = new int[]{-1,1,0,0};
+        int[] dirY = new int[]{0,0,-1,1};
+        int ans = 0;
+        while(!queue.isEmpty()){
+            Cell cell = queue.pollFirst();
+            ans++;
+            for(int i = 0; i < 4; i++){
+                int nrow = cell.row + dirX[i];
+                int ncol = cell.col + dirY[i];
+                if(check(nrow, ncol, grid)){
+                    grid[nrow][ncol] = -1;
+                    queue.offerLast(new Cell(nrow, ncol));
+                }
+            }
+        }
+        return ans;
+    }
+    private int gridDfs(int[][] grid, int row, int col){
+        grid[row][col] = -1;
+        int[] dirX = new int[]{-1,1,0,0};
+        int[] dirY = new int[]{0,0,-1,1};
+        int ans = 0;
+        for(int i = 0; i < 4; i++){
+            int nrow = row + dirX[i];
+            int ncol = col + dirY[i];
+            if(check(nrow, ncol, grid)){
+                ans = ans + gridDfs(grid, nrow, ncol);
+            }
+        }
+        return 1 + ans;
+    }
+    public int maxAreaOfIsland(int[][] grid) {
+        int ans = 0;
+        ArrayDeque<Cell> queue = new ArrayDeque<>();
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j++){
+                if(grid[i][j] == 1){
+                    ans = Math.max(ans, gridBfs(grid, i, j, queue));
+                    // ans = Math.max(ans, gridDfs(grid, i, j));
+                }
+            }
+        }
+        return ans;
+    }
+}
