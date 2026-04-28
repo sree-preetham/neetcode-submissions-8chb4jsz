907d88bfda685586a06730010cb24b8b59234fba
Sree Preetham <76846180+sree-preetham@users.noreply.github.com>
2026-04-28 20:43:46 +0900

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    vector<vector<int>> buildAdj(vector<vector<int>>& edges, int n){
+        vector<vector<int>> adj(n);
+        for(const vector<int> &edge: edges){
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+        return adj;
+    }
+    bool dfsCheckCycle(int src, vector<vector<int>>& adj, vector<int> &vis, vector<int> &dfsVis){
+        vis[src] = true;
+        for(int &neighbour: adj[src]){
+            if(dfsVis[neighbour]) return true;
+            else if (!vis[neighbour]) if(dfsCheckCycle(neighbour, adj, vis, dfsVis)) return true;
+        }
+        dfsVis[src] = true;
+        return false;
+    }
+    bool dfsOptimized(int src, vector<vector<int>>& adj, vector<int> &vis, int parent){
+        vis[src] = true;
+        for(int &neighbour: adj[src]){
+            if (!vis[neighbour]) {
+                if(dfsOptimized(neighbour, adj, vis, src)) return true;
+            }
+            else if(neighbour!=parent) return true;
+        }
+        return false;
+    }
+    bool bfsCheckCycle(int src, vector<vector<int>>& adj, vector<int> &vis){
+        queue<pair<int,int>> q;
+        q.push({src, -1});
+        vis[src] = true;
+        while(!q.empty()){
+            auto [node, parent] = q.front();
+            q.pop();
+            for(int &neighbour: adj[node]){
+                if (!vis[neighbour]) {
+                    vis[neighbour] = true;
+                    q.push({neighbour, node});
+                }
+                else if(neighbour!=parent) return true;
+            }
+        }
+        return false;
+    }
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if(edges.size()!=n-1) return false;
+        // cycle
+        vector<vector<int>> adj = buildAdj(edges, n);
+        vector<int> vis(n, false);
+        vector<int> dfsVis(n, false);
+        int component = 0;
+        bool ans = true;
+        for(int i = 0; i < n; i++){
+            if(!vis[i]) {
+                // ans = !dfsCheckCycle(i, adj, vis, dfsVis);
+                // ans = !dfsOptimized(i, adj, vis, -1);
+                ans = !bfsCheckCycle(i, adj, vis);
+                component++;
+            }
+        }
+        return ans && (component == 1);
+    }
+};
